db722d1c2e242751ac267798f5d9a80c58ed4612
esther-inyang <estherinyang07@gmail.com>
2023-02-16 17:30:17 +0100

    page routing

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+
+import style from "../styles/Home.module.css"
+
+function about() {
+  return (
+    <h1 className={style.aboutPageTitle}>About page</h1>
+  )
+}
+
+export default about
---
+++ pages/profile.js
@@ -0,0 +1,10 @@
+
+import style from "../styles/Home.module.css"
+
+function profile() {
+  return (
+    <h1 className={style.profilePageTitle}>Profile Page</h1>
+  )
+}
+
+export default profile
